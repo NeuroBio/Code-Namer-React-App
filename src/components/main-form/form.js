c5e1ff469a7e78214f8b3af012f1879a4b57bee8
kArA <kArARedwing@gmail.com>
2023-03-12 10:00:12 -0400

    add sarcasm

--- src/components/main-form/form.js
+++ src/components/main-form/form.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FormInput } from '../form-input/form-input';
+import './form.css';
 
 export class Form extends React.Component {
     constructor(props) {
@@ -21,7 +22,21 @@ export class Form extends React.Component {
 
     render () {
         return (<form onSubmit={this.submitHandler}>
-            Henlo
+            <section className="long-text" id="mission-statement">
+                <p>
+                    <b>Your mission:</b> to never get your PR rejected over naming conventions ever again.
+                </p>
+
+                <p>
+                    How do we achieve this?  With the combined power of automation and science!!  Just answer a few quick questions for our <i>state-of-the-art, natural-language machine learning algorithm</i> to never be questioned by a fleshy humanoid again!
+                </p>
+
+                <p>
+                    <span className="smol">On naming.</span><br />
+                    <span className="smoller">You'll still be questioned on literally everything else.</span><br />
+                    <span className="smollest">Including why you chose to wear <i>those</i> shoes today.</span>
+                </p>
+            </section>
 
             <FormInput label="Test" onUpdate={this.updateTest}/>
 
